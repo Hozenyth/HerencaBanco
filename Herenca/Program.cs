2d5307f62e4b3683708d82af47b197dae699ae5f
Hozenyth Andrade <hozenyth.andrade@gmail.com>
2021-08-27 19:36:32 -0300

    Adicionar arquivos de projeto.

---
+++ Herenca/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; 
+
+namespace Heranca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // var conta = new ContaCorrente(20);
+
+            
+        }
+    }
+}
